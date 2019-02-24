2c1588600c21dd1b6994088715e58fd4650c91a2
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-24 20:05:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,11 @@ def using_push(array, x)
   array.push(x)
 end
 
-def using_unshift
-  neighborhoods_in_northwest_brooklyn.unshift
+def using_unshift(array, x)
+  array.unshift(x)
 end
+
+def using_pop(array)
+  array.pop
+end
+
